7e580cfc69fff0ae41f07b3db8c83c72c7abb475
kora11e <87006301+kora11e@users.noreply.github.com>
2024-05-21 15:08:26 +0200

    update

---
+++ test.py
@@ -0,0 +1,12 @@
+ls = [0, 0, 5, 64, 64, 33, 25, 12, 0, 0, 99, 25, 22, 11, 90, 121, 11, 64, 0, 0]
+
+output = [0] * len(ls)
+
+output[0] = ls[0]
+
+for i in range(1, len(ls)):
+    print(ls[i])
+
+def function(ls, output):
+    # Implement Strassen's Matrix Multiplication here
+    pass
